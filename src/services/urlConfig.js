3fcf34dee50f80e4127d3b483a9848e63d1faf5d
ngkhanhduy98 <57517017+ngkhanhduy98@users.noreply.github.com>
2024-10-10 09:31:22 +0700

    Update urlConfig.js

--- src/services/urlConfig.js
+++ src/services/urlConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = "https://fiverrnew.cybersoft.edu.vn";
 export const TOKEN_CYBER =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2NCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsIm5iZiI6MTY5NTkyMDQwMCwiZXhwIjoxNzI1Mjk2NDAwfQ.36nQu-fyhBElKov0sWvrvwuO832nQWmfRIHcRVPB7Mw";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2NCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODkwMDAwMCIsIm5iZiI6MTY5NTkyMDQwMCwiZXhwIjoxNzI1Mjk2NDAwfQ.l86LVJIHAEP4MrkrVI_P3SmcSZj5fQv7BKwh3Mm43EA";
 export const http = axios.create({
   baseURL: BASE_URL,
   headers: {
